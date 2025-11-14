dcd43d3b3354a61324ebe9a702b18305ce5ffe55
Yuuuki16 <cp0268ki@ed.ritsumei.ac.jp>
2025-11-14 21:16:17 +0900

    新しいファイル

---
+++ android/app/src/main/kotlin/com/example/watnow_autumn_team3_front/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.watnow_autumn_team3_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- watnow_autumn_team3_frot/android/app/src/main/kotlin/com/example/watnow_autumn_team3_frot/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.watnow_autumn_team3_frot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
