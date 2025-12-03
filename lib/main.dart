0ec289276344357adc5cc84c245783b2bcd9717f
inoueeeee2025 <0nc59f4738c1r6a@gmail.com>
2025-12-04 01:00:27 +0900

    画面のフォーマット作成と0per画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,144 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      backgroundColor: const Color(0xFFFAF9F6),
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Stack(
+          children: [
+            // 下のレイヤー：今までの Column（ちょっとだけ上の余白を増やす）
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 140), // ← タイトル行ぶん少し多めに空ける
+
+                const Text(
+                  "達成度",
+                  style: TextStyle(
+                    fontSize: 55,
+                    color: Color(0xD9006400),
+                    fontFamily: 'Building',
+                  ),
+                ),
+
+                const SizedBox(height: 4),
+
+                const Text(
+                  "0%",
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xD9006400),
+                    fontFamily: 'Building',
+                  ),
+                ),
+
+                const SizedBox(height: 40),
+
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Image.asset('assets/images/0per.png'),
+                  ),
+                ),
+              ],
+            ),
+
+            Positioned(
+              top: 40, // ← 上からの距離
+              left: 28, // ← 左からの距離
+              right: 16, // ← 右まで使う（Rowの両端に配置したいのでrightも指定）
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "ホーム",
+                    style: TextStyle(
+                      color: Color(0xD9006400),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 38,
+                      fontFamily: 'Building',
+                      shadows: const [
+                        Shadow(
+                          color: const Color.fromRGBO(0, 0, 0, 0.25),
+                          offset: const Offset(0, 3),
+                          blurRadius: 6,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/aikon.png',
+                    width: 42, // ← FigmaのW
+                    height: 42, // ← FigmaのH
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      //フッターの設定これで統一してほしい
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          color: const Color(0xA8006400),
+          height: 80,
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // 左の余白
+              SvgPicture.asset(
+                'assets/icons/icon_group.svg',
+                width: 53,
+                height: 53,
+              ),
+
+              const SizedBox(width: 34),
+
+              SvgPicture.asset(
+                'assets/icons/icon_home.svg',
+                width: 50,
+                height: 50,
+              ),
+
+              const SizedBox(width: 34),
+
+              SvgPicture.asset(
+                'assets/icons/icon_list.svg',
+                width: 50,
+                height: 50,
+              ),
+
+              const SizedBox(width: 34),
+
+              SvgPicture.asset(
+                'assets/icons/icon_insight.svg',
+                width: 50,
+                height: 50,
+              ),
+              // 右の余白
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
