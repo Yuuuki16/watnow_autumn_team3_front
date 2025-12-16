5654f0353aec7ce11a1426599a517863d52b1034
Yuuuki16 <cp0268ki@ed.ritsumei.ac.jp>
2025-12-17 00:18:05 +0900

    タブでの画面遷移

--- lib/pages/home/insight_page.dart
+++ lib/pages/home/insight_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
-import '../insight/insight_all_view.dart';
+import '../insight/insight_all_view.dart' as all_view;
+import '../insight/insight_week_view.dart' as week_view;
 
 class InsightPage extends StatelessWidget {
   const InsightPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // insight_all_view.dart の VerticalPageViewScreen をそのまま表示する
-    return const VerticalPageViewScreen();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 1,
+          title: const Text('インサイト', style: TextStyle(color: Colors.black)),
+          bottom: const TabBar(
+            tabs: [Tab(text: '全体'), Tab(text: '週間')],
+            labelColor: Colors.green,
+            unselectedLabelColor: Colors.grey,
+            indicatorColor: Colors.green,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            const all_view.VerticalPageViewScreen(),
+            const week_view.VerticalPageViewScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }
